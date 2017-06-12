a2322232ebf2e76d0561f206c84e0e733f366911
Bailey Kursar <bkursar@gmail.com>
2017-06-12 11:13:43 +0100

    Set up more extensive formatting for rspec, including test coverage, and fixed all Rubocop offenses

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,17 +1,15 @@
 require 'account'
 
+## User Story 1:
+## As a client,
+## So I can save and spend money,
+## I need to have a bank account.
+
 describe Account do
-
-  describe '#account', %q{
-    As a client,
-    So I can save and spend money,
-    I need to have a bank account.
-    } do
-
+  describe '#account' do
     it 'initializes with a client name' do
       account = Account.new('Client')
       expect(account.name).to eq 'Client'
     end
   end
-
 end
